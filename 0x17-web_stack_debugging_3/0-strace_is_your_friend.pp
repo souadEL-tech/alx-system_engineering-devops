828ac508fc41fb4efae567e6eafa49b1dd0ea576
Souad-Tech <sou.elmarsani@gmail.com>
2024-08-17 23:49:16 +0000

    0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# automated puppet fix (to find out why apache is returning a 500 error)
+
+
+exec { 'fix wordpress site':
+  command => 'udo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
