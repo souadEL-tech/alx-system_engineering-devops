4ae9cc35fdcc4b9fd9de7416330c429d46aa6530
souad_tech <sou.elmarsani@gmail.com>
2024-08-25 12:04:13 -0700

    1-top_ten.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,18 +12,13 @@ def number_of_subscribers(subreddit):
     """Set up the URL for the Reddit API endpoint for the subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Custom User-Agent"}
-    
+
     try:
-        # Make a GET request to the subreddit about endpoint
         response = requests.get(url, headers=headers, allow_redirects=False)
-        # Check if the request was successful
-        if response.status_code == 200:
+        if response.status_code == 404:
             data = response.json()
-            # Extract the number of subscribers
             return data['data']['subscribers']
         else:
-            # If the subreddit is invalid or any error occurs, return 0
             return 0
     except requests.RequestException:
-        # If there is a network-related error, return 0
         return 0
