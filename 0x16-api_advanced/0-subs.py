3ed7a8094c5a52f6eb7083a8e015a566def4ad9b
souad_tech <sou.elmarsani@gmail.com>
2024-09-13 15:22:42 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,21 +4,19 @@
 Function to query subscribers on a given Reddit subreddit
 """
 
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Set up the URL for the Reddit API endpoint for the subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Custom User-Agent"}
-
+    """ Return the total number of subscribers on given subressit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 404:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+        results = response.json().get("data")
+        return (results.get("subscribers", 0))
+    except ValueError:
+        print("Error: Could not decode the response as JSON")
         return 0
